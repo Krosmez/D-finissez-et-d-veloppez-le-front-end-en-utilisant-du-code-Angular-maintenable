4a5083f9e0ba454bdbdf78aaeb05fbb5b855053b
Krosmez <jonathanmaison78@gmail.com>
2026-01-22 20:15:36 +0100

    refactor: add models

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import Chart from 'chart.js/auto';
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Olympic } from 'src/app/models/Olympic';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,61 +11,105 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
   private olympicUrl = './assets/mock/olympic.json';
-  public pieChart!: Chart<"pie", number[], string>;
-  public totalCountries: number = 0
-  public totalJOs: number = 0
-  public error!: string
-  titlePage: string = "Medals per Country";
+  public pieChart!: Chart<'pie', number[], string>;
+  public totalCountries: number = 0;
+  public totalJOs: number = 0;
+  public error!: string;
+  titlePage: string = 'Medals per Country';
 
-  constructor(private router: Router, private http: HttpClient) { }
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+  ) {}
 
   ngOnInit() {
-    this.http.get<any[]>(this.olympicUrl).pipe().subscribe(
-      (data) => {
-        console.log(`Liste des données : ${JSON.stringify(data)}`);
-        if (data && data.length > 0) {
-          this.totalJOs = Array.from(new Set(data.map((i: any) => i.participations.map((f: any) => f.year)).flat())).length;
-          const countries: string[] = data.map((i: any) => i.country);
-          this.totalCountries = countries.length;
-          const medals = data.map((i: any) => i.participations.map((i: any) => (i.medalsCount)));
-          const sumOfAllMedalsYears = medals.map((i) => i.reduce((acc: any, i: any) => acc + i, 0));
-          this.buildPieChart(countries, sumOfAllMedalsYears);
-        }
-      },
-      (error: HttpErrorResponse) => {
-        console.log(`erreur : ${error}`);
-        this.error = error.message
-      }
-    )
+    this.http
+      .get<Olympic[]>(this.olympicUrl)
+      .pipe()
+      .subscribe(
+        (data: Olympic[]) => {
+          if (data && data.length > 0) {
+            this.totalJOs = Array.from(
+              new Set(
+                data
+                  .map((olympic: Olympic) =>
+                    olympic.participations.map(
+                      (participation) => participation.year,
+                    ),
+                  )
+                  .flat(),
+              ),
+            ).length;
+
+            const countries: string[] = data.map(
+              (olympic: Olympic) => olympic.country,
+            );
+
+            this.totalCountries = countries.length;
+
+            const medals = data.map((olympic: Olympic) =>
+              olympic.participations.map(
+                (participation) => participation.medalsCount,
+              ),
+            );
+            const sumOfAllMedalsYears = medals.map((medalCounts) =>
+              medalCounts.reduce(
+                (acc: number, count: number) => acc + count,
+                0,
+              ),
+            );
+            this.buildPieChart(countries, sumOfAllMedalsYears);
+          }
+        },
+
+        (error: HttpErrorResponse) => {
+          this.error = error.message;
+        },
+      );
   }
 
   buildPieChart(countries: string[], sumOfAllMedalsYears: number[]) {
-    const pieChart = new Chart("DashboardPieChart", {
+    const pieChart = new Chart('DashboardPieChart', {
       type: 'pie',
       data: {
         labels: countries,
-        datasets: [{
-          label: 'Medals',
-          data: sumOfAllMedalsYears,
-          backgroundColor: ['#0b868f', '#adc3de', '#7a3c53', '#8f6263', 'orange', '#94819d'],
-          hoverOffset: 4
-        }],
+        datasets: [
+          {
+            label: 'Medals',
+            data: sumOfAllMedalsYears,
+            backgroundColor: [
+              '#0b868f',
+              '#adc3de',
+              '#7a3c53',
+              '#8f6263',
+              'orange',
+              '#94819d',
+            ],
+            hoverOffset: 4,
+          },
+        ],
       },
       options: {
         aspectRatio: 2.5,
         onClick: (e) => {
           if (e.native) {
-            const points = pieChart.getElementsAtEventForMode(e.native, 'point', { intersect: true }, true)
+            const points = pieChart.getElementsAtEventForMode(
+              e.native,
+              'point',
+              { intersect: true },
+              true,
+            );
             if (points.length) {
               const firstPoint = points[0];
-              const countryName = pieChart.data.labels ? pieChart.data.labels[firstPoint.index] : '';
+              const countryName = pieChart.data.labels
+                ? pieChart.data.labels[firstPoint.index]
+                : '';
               this.router.navigate(['country', countryName]);
             }
           }
-        }
-      }
+        },
+      },
     });
     this.pieChart = pieChart;
   }
 }
-
